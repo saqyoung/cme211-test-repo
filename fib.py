b32b29417e3374820af8d47c4cab42be3642abe2
Sean Young <saqyoung@stanford.edu>
2018-01-14 20:15:23 -0800

    simple python scripts

---
+++ fib.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.argv[1])
+
+if n >= 0:
+	fn2 = 0
+	print (fn2,end=',')
+if n >= 1:
+	fn1 = 1
+	print(fn1,end=',')
+for i in range(2,n+1):
+	fn = fn1 + fn2
+	print(fn,end=',')
+	fn2 = fn1
+	fn1 = fn
+print()
---
+++ firstscript.py
@@ -0,0 +1,2 @@
+print("Hello from Python")
+print("I am your first script!")
